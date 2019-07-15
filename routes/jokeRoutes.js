16335e9c76e7f65873b5e3c2b15c0367ab612806
Dexter Lewis <dexter.lewis@gmail.com>
2019-07-15 19:42:28 -0400

    add authorization middleware to jokes routes

--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -1,24 +1,45 @@
 const express = require('express')
+const Jokes = require('../data/models/jokes')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 
-// Returns all jokes
-router.get('/', (req, res) => {
-    res.send('Returns all jokes')
+// Authorization middleware
+
+function isAuthorized (req, res, next) {
+    const token = req.headers.authorization
+    if(token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+            if(err) {
+                res.status(403).json({message: 'You are not authorized'})
+            } else {
+                next()
+            }
+        })
+    } else {
+        res.status(400).json({message: 'No credentials provided'})
+    }
+}
+
+
+// Returns all public jokes
+router.get('/', async (req, res) => {
+    const jokes = await Jokes.getPublic()
+    res.status(200).json(jokes)
 })
 
 // TODO: add validation to protect route
-router.post('/:id/joke', (req, res) => {
+router.post('/:id/joke', isAuthorized, (req, res) => {
     res.send('Adds new joke')
 })
 
 // TODO: add validation to protect route
-router.put('/:id', (req, res) => {
+router.put('/:id', isAuthorized, (req, res) => {
     res.send('Update jokes')
 })
 
 // TODO: add validation to protect route
 // Deletes joke
-router.delete('/:id', (req, res) => {
+router.delete('/:id', isAuthorized, (req, res) => {
     res.send('Removed Joke')
 })
 
