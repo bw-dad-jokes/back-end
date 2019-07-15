57f06198895452f032b611975e32b132f184bd25
Dexter Lewis <dexter.lewis@gmail.com>
2019-07-15 19:49:04 -0400

    Update welcome message when new account is created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,7 +25,7 @@ router.post('/login', async (req, res) => {
     const user = await User.getByUsername(username);
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = generateToken(user);
-      res.status(200).json({ username: user.username, authToken: token });
+      res.status(200).json({ username: user.username, userId: user.id, authToken: token });
     } else {
       res.status(400).json({ message: 'Invalid username or password' });
     }
@@ -37,11 +37,8 @@ router.post('/login', async (req, res) => {
 router.post('/signup', checkUserName, async (req, res) => {
   try {
     let user = req.body;
-    const token = await generateToken(user);
-    const hash = bcrypt.hashSync(user.password, 12);
-    user.password = hash;
     user = await User.insert(user);
-    res.status(201).json({ username: user.username, authToken: token });
+    res.status(201).json({ username: `Welcome ${user.username}` });
   } catch (err) {
     res.status(500).json({ message: 'Database is unavilable' });
   }
