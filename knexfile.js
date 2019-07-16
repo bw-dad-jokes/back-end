57fa5bc498307815e61634196fcf8de975d5a108
Dexter Lewis <dexter.lewis@gmail.com>
2019-07-15 21:43:48 -0400

    Add files for pg deployment

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
-  }
+  },
 
   // staging: {
   //   client: 'postgresql',
@@ -42,20 +42,17 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DATABASE_URL,
+      // user:     'username',
+      // password: 'password'
+    },
+    migrations: {
+      directory: './data/migrations'
+    }, 
+    useNullAsDefault: true
+  }
 
 };
