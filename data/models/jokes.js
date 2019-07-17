8b9174a9c9de57d8ce71e9298c3da48331418abb
kaiserawu <kaiser.a.wu@gmail.com>
2019-07-16 20:41:45 -0700

    Updated getAllPublicJokes function

--- data/models/jokes.js
+++ data/models/jokes.js
@@ -8,38 +8,29 @@ async function getPublic() {
     .join('keywords', 'keywords.id', 'jokes_keywords.keyword_id')
     .select('jokes.*', 'keywords.keyword')
     .where({ public: true });
-  
-  let tempJokeObj = {};
-  let currId = publicJokesAndKeywords[0].id;
-  let concattedJokes = [];
 
-  function makeNewTempJokeObj(item) {
-      tempJokeObj = {
-        id: item.id,
-        joke_text: item.joke_text,
-        public: item.public,
-        private: item.private,
-        user_id: item.user_id,
-        keywords: [item.keyword]
-      };
-  }
+  let concattedJokesObj = {};
 
   for (let i = 0; i < publicJokesAndKeywords.length; i++) {
     let currItem = publicJokesAndKeywords[i];
-    if (!tempJokeObj.id) {
-      makeNewTempJokeObj(currItem);
-    } else if (currItem.id === tempJokeObj.id) {
-      tempJokeObj.keywords.push(currItem.keyword);
+    let currId = currItem.id;
+    if (!concattedJokesObj[currId]) {
+      concattedJokesObj[currId] = {
+        id: currItem.id,
+        joke_text: currItem.joke_text,
+        public: currItem.public,
+        private: currItem.private,
+        user_id: currItem.user_id,
+        keywords: [currItem.keyword]
+      };
     } else {
-      concattedJokes.push(tempJokeObj);
-      makeNewTempJokeObj(currItem);
-    }
-    if (i === publicJokesAndKeywords.length - 1) {
-      concattedJokes.push(tempJokeObj);
+      concattedJokesObj[currId].keywords.push(currItem.keyword);
     }
   }
 
-  return concattedJokes;
+  return Object.keys(concattedJokesObj).map(key => {
+    return concattedJokesObj[key];
+  });
 }
 
 function deleteById(id) {
