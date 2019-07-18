a20c000144b009fcffae770e15fcca4922b73c2d
Dexter Lewis <dexter.lewis@gmail.com>
2019-07-17 20:27:48 -0400

    Updated code formatting

--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Jokes = require('../data/models/jokes');
 const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator')
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 // Authorization middleware
@@ -13,6 +13,7 @@ function isAuthorized(req, res, next) {
       if (err) {
         res.status(403).json({ message: 'You are not authorized' });
       } else {
+        req.userId = payload.userId;
         next();
       }
     });
@@ -21,7 +22,6 @@ function isAuthorized(req, res, next) {
   }
 }
 
-
 // Returns all public jokes
 router.get('/', async (req, res) => {
   const jokes = await Jokes.getPublic();
@@ -29,68 +29,72 @@ router.get('/', async (req, res) => {
 });
 
 // Add a new joke
-router.post('/', isAuthorized, [
-  check('user_id').exists(),
-  check('joke_text').exists(),
-  check('private').exists(),
-  check('private').exists()
-], async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      return res.status(422).json(errors.array())
-    } else {
-
-      const joke = req.body;
-      const newJoke = await Jokes.addJoke(joke);
-      res.status(201).json(newJoke);
+router.post(
+  '/',
+  isAuthorized,
+  [
+    check('user_id').exists(),
+    check('joke_text').exists(),
+    check('private').exists(),
+    check('private').exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json(errors.array());
+      } else {
+        const joke = req.body;
+        const newJoke = await Jokes.addJoke(joke);
+        res.status(201).json(newJoke);
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Server is unavailable' });
     }
+  }
+);
+
+router.put(
+  '/:id',
+  isAuthorized,
+  [
+    check('user_id').exists(),
+    check('joke_text').exists(),
+    check('private').exists(),
+    check('private').exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json(errors.array());
+      } else {
+        const id = req.params.id;
+        const joke = req.body;
+        const updatedJoke = await Jokes.updateJoke(id, joke);
+        if (!updatedJoke) {
+          return res.status(404).json({ message: 'The joke does not exist' });
+        }
+        res.status(200).json(updatedJoke);
+      }
+    } catch (err) {
+      res.status(500).json({ err: err.message });
+    }
+  }
+);
+
+// Deletes joke
+router.delete('/:id', isAuthorized, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const joke = await Jokes.deleteJoke(id);
+    if (!joke) {
+      return res.status(400).json({ message: 'Joke does not exist' });
+    }
+    res.status(200).json(joke);
   } catch (err) {
     res.status(500).json({ message: 'Server is unavailable' });
   }
 });
-
-
-router.put('/:id', isAuthorized, [
-  check('user_id').exists(),
-  check('joke_text').exists(),
-  check('private').exists(),
-  check('private').exists()
-], async (req, res) => {
-  try{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      return res.status(422).json(errors.array())
-    } else {
-
-      const id = req.params.id
-      const joke = req.body
-      const updatedJoke = await Jokes.updateJoke(id, joke);
-      if(!updatedJoke) {
-        return res.status(404).json({message: 'The joke does not exist'})
-      }
-      res.status(200).json(updatedJoke);
-    }
-
-  } catch(err) {
-    res.status(500).json({ err: err.message });
-  }
-  
-});
-
-// TODO: add validation to protect route
-// Deletes joke
-router.delete('/:id', isAuthorized, async (req, res) => {
-  try {
-    const id = req.params.id
-    const joke = await Jokes.deleteJoke(id)
-    if(!joke) {
-      return res.status(400).json({ message: 'Joke does not exist'})
-    }
-    res.status(200).json(joke)
-  } catch(err) {
-    res.status(500).json({ message: 'Server is unavailable' });
-  }
-})
 
 module.exports = router;
